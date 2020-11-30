dfde323e93513bf88e23f8545bfc05dbd3059021
yang <developerlily@hotmail.com>
2020-11-30 22:03:53 +1100

    page created

---
+++ app/code/BiFang/OrderTracker/Block/Main.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ */
+namespace BiFang\OrderTracker\Block;
+
+/**
+ * Class Main
+ */
+class Main extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    function _prepareLayout(){}
+}
---
+++ app/code/BiFang/OrderTracker/view/frontend/requirejs-config.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+var config = {
+    map: {
+        "*": {
+            "mapbox": "BiFang_OrderTracker/js/mapbox",
+            "mapboxGL": "BiFang_OrderTracker/js/mapbox/mapbox-gl",
+        }
+    },
+};
